24b8d8cbbb4535023ff12dbdad92b289b516c088
Scott Matthewman <scott@matthewman.net>
2023-04-13 10:08:26 +0100

    Add a CardView

---
+++ Sources/TheatriCalViews/CardView/CardView.swift
@@ -0,0 +1,105 @@
+//
+//  CardView.swift
+//  
+//
+//  Created by Scott Matthewman on 13/04/2023.
+//
+
+import SwiftUI
+
+
+public struct CardView<Image: View, Overlay: View, Content: View>: View {
+    @Environment(\.colorScheme) private var colorScheme
+
+    var cornerRadius: CGFloat = 10
+    @ScaledMetric(relativeTo: .body) var spacing = 6
+
+    @ViewBuilder var image: () -> Image
+    @ViewBuilder var overlay: () -> Overlay
+    @ViewBuilder var content: () -> Content
+
+    public init(
+        cornerRadius: CGFloat = 10,
+        spacing: Double = 6,
+        @ViewBuilder image: @escaping () -> Image,
+        @ViewBuilder overlay: @escaping () -> Overlay,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.cornerRadius = cornerRadius
+        _spacing = ScaledMetric(wrappedValue: spacing, relativeTo: .body)
+        self.image = image
+        self.overlay = overlay
+        self.content = content
+    }
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            image()
+                .overlay(alignment: .bottomLeading) {
+                    VStack(alignment: .leading, spacing: spacing) {
+                        overlay()
+                    }
+                    .padding(spacing)
+                }
+            VStack(alignment: .leading, spacing: spacing) {
+                content()
+            }
+            .padding(spacing)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+        .background(cardBackground)
+        .contentShape(Rectangle())
+    }
+
+    var cardBackground: some View {
+        RoundedRectangle(cornerRadius: cornerRadius)
+            .foregroundColor(
+                colorScheme == .dark ? .primary : Color(uiColor: .systemBackground)
+            )
+            .opacity(colorScheme == .dark ? 0.1 : 1.0)
+            .shadow(radius: 5)
+    }
+}
+
+extension CardView {
+    public init(
+        cornerRadius: CGFloat = 10,
+        spacing: Double = 6,
+        @ViewBuilder image: @escaping () -> Image,
+        @ViewBuilder content: @escaping () -> Content
+    ) where Overlay == EmptyView {
+        self.cornerRadius = cornerRadius
+        _spacing = ScaledMetric(wrappedValue: spacing, relativeTo: .body)
+        self.image = image
+        self.overlay = { EmptyView() }
+        self.content = content
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            CardView {
+                Image("play-photo")
+                    .resizable()
+                    .scaledToFit()
+            } overlay: {
+                Image(systemName: "theatermasks.fill")
+                    .foregroundStyle(.white)
+            } content: {
+                Text("Shakespeare for Breakfast")
+                    .font(.title)
+            }
+
+            CardView {
+                Image("play-photo")
+                    .resizable()
+                    .scaledToFit()
+            } content: {
+                Text("Shakespeare for Breakfast")
+                    .font(.title)
+            }
+        }
+        .padding()
+    }
+}
